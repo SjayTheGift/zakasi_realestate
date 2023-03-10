13504d9c3e02728d2808da8202deecbcbeaf712c
Esethu <jonas.esethu24@gmail.com>
2023-03-10 13:02:02 +0200

    hide secrect key

--- zakasi_realestate/settings.py
+++ zakasi_realestate/settings.py
@@ -20,12 +20,12 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# env = environ.Env(
-#     # set casting, default value
-#     DEBUG=(bool, False)
-# )
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
-# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 
@@ -33,13 +33,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6h%j8f&53$#2whd531o4(ekywf44bs*9-j&p4j_#&y542cdd0a'
+
 
 DEBUG = False
 
-# DEBUG = env('DEBUG')
+DEBUG = env('DEBUG')
 
-# SECRET_KEY=env("SECRET_KEY")
+SECRET_KEY=env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
